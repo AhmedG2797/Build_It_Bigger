623854ccc9c32a5d4f9d36280b4f39ca1b9b08a8
Ahmed Gamal <ahmedG2797@gmail.com>
2019-04-10 01:54:03 +0200

    first

---
+++ androidlib/src/androidTest/java/com/ahmedg2797/androidlib/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.ahmedg2797.androidlib;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.ahmedg2797.androidlib", appContext.getPackageName());
+    }
+}
---
+++ androidlib/src/main/java/com/ahmedg2797/androidlib/AppConstants.java
@@ -0,0 +1,5 @@
+package com.ahmedg2797.androidlib;
+
+public class AppConstants {
+    public static final String JOKE_KEY = "joke";
+}
